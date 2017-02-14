e6160d4b87df2101b0add5ebffa2331087a3061e
nyt <jonsspamaccout@gmail.com>
2017-02-14 11:54:09 -0800

    cleanup

--- factornum.py
+++ factornum.py
@@ -29,93 +29,74 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+from __future__ import print_function
+try:
+    input = raw_input # python2
+except NameError:
+    pass # python 3
+
 import math
 import time
- 
 
 def is_square(number):
-    perfect_square = math.sqrt(number)    #takes the square root of the users number to see if its a perfect square
-    square_check = number % perfect_square    #checks to see if the users number is a perfect square
-    
-    if square_check == 0:
-        return True
-    else:
-        return False
+    """checks if a number is a perfect square"""
+    square_root = math.sqrt(number)    #takes the square root of the users number to see if its a perfect square
+    return int(square_root) == number #checks to see if the users number is a perfect square
 
-def factor(number):   
-    """Takes in one int, returns a list of factors"""
-    
-    factors = []
-    
-    if number <= 0:
-        return ('Invalid number')
-    else:
-        number_count = int(1)    #sets the number count = to 1
-        factor_a = (2)    #defines the initial factor a which will be redefined in the first loop
-    
-        
-        if is_square(number):    #this is the factoring loop for perfect squares, it displays the number*itself at the end
-            while number_count <= factor_a:
-                 if number % number_count == 0:
-                    factor_a = (number /int(number_count))    #devides number by number_count to get both factors
-                    factors.append(number_count)
-                    factors.append(factor_a)
-                 number_count += 1    #advances the count by 1
-        else:    #this is the loop for non-perfect squares
-            while number_count < factor_a:    #this is the only line that changes, as it doesnt diplay the number*itself at the end
-                 if number % number_count == 0:
-                    factor_a = (number / number_count)
-                    factors.append(number_count)
-                    factors.append(factor_a)
-                 number_count += 1
-        return factors
+def factor(number):
+    """Takes in one int, returns a generator of factors"""
+
+    if number < 1:
+        raise ValueError("Number must be greater than zero")
+
+    number_count = 1    #sets the number count = to 1
+    factor_a = 2    #defines the initial factor a which will be redefined in the first loop
+    while number_count < factor_a:    #this is the only line that changes, as it doesnt diplay the number*itself at the end
+        if number % number_count == 0:
+            factor_a = (number // number_count)
+            yield number_count
+            yield factor_a
+        number_count += 1
 
 def print_factors(number):
     """Takes a one int, factors it, then
      prints the list of factors to the console in a user readable format"""
-    
     factors = factor(number)
-    
-    if factors == 'Invalid number':
-        print("Invalid number, please try again")
-    else:
-        for i in range(0, int((len(factors))), 2):
-            print (factors[i], factors[i+1])
+    for i in factors:
+        print("{:<4}{}".format(i, next(factors)))
 
 def is_prime(number):
     """Takes in one int, returns True if prime or False if not"""
-    
-    factors = factor(number)
-    if len(factors) == 2:
-        return True
-    else:
-        return False
+    next(factors) # throw out the first 2 factors
+    next(factors)
+    for _ in factors:
+        return False # as soon as a 3rd factor is found, return False
+    return True
+
+def cli():
+    try:
+        number = int(input('Please enter number to recieve factors: '))  #this is the users number to be factored
+        print_factors(number)
+    except ValueError as e:
+        #~ print("Please enter a positive integer")
+        print("ERROR:", e)
+
+def repeat():
+    while True:
+        u_input = input('Would you like another number factored? \n Yes or No:')    #prompts user, gives options, and takes user input
+        if u_input.lower().startswith('n'):    #defines the end command
+            return False
+        elif u_input.lower().startswith('y'):   #defines the continue command
+            return True
+        else:    #loops the program back if the input isnt expected
+            print ('Sorry, try again')
 
 def main():
-    master = True
-    while master:
-    
-        number = int(input('Please enter number to recieve factors: '))  #this is the users number to be factored 
-        print_factors(number)
-        
-        end = True    #establishes the end loop counter
-        while end:
-            u_input = str(input('Would you like another number factored? \n Yes or No:'))    #prompts user, gives options, and takes user input
-        
-            if u_input.lower() == 'no' or u_input.lower() == 'n':    #defines the end command 
-                master = False
-                end = False
-        
-            elif u_input.lower() == 'yes' or u_input.lower() == 'y':   #defines the continue command
-                end = False
-        
-            else:    #loops the program back if the input isnt expected
-                print ('Sorry, try again')
-    
+    while True:
+        cli()
+        if not repeat():
+            break
     print ('Thanks for using')    #displays an ending message
-    time.sleep(1)
-
-
 
 if __name__=='__main__':
     main()
